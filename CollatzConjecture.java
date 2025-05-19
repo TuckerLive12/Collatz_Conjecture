d12ce271ef899cf9dd8253cba5facb234367afbc
TuckerLive12 <Jack.kucera@me.com>
2025-05-19 15:17:43 -0500

    added new efficent method for testing as many collatz numbers as possible.

--- CollatzConjecture.java
+++ CollatzConjecture.java
@@ -15,7 +15,11 @@ public class CollatzConjecture {
    * This method adds a new node at the beginning of LinkedList with the value inputed. 
    *  Increases size by one.
    *
-   * @param data, an integer to put into the new node
+   * @param start, A long which indicates what number to start testing the Collatz Conjecture
+   * @param end, A long which indicates what number to end testing the Collatz Conjecture. If end is 0, never stops.
+   * @param write, A boolean (Will you finish this)
+   * @param showEachOperation, A boolean indicating whether to print each operation
+   * @throws IOException, If an IOExcpetion has taken place
    * @return void
    */
     public static void collatzConjecture(long start, long end, boolean write, boolean showEachOperation) throws IOException {
@@ -53,4 +57,58 @@ public class CollatzConjecture {
 		}
 		fwriter.close();
     }
+
+	/**
+   * This method... The method assumes that the numbers before the starting number have been checked (word better).
+   *
+   * @param start, A long which indicates what number to start testing the Collatz Conjecture
+   * @param end, A long which indicates what number to end testing the Collatz Conjecture. If end is 0, never stops.
+   * @param iterationToPrint, An int which indicates after how many iterations to print the numbering being tested. Does this by modulating the number being tested, if 0, print. Recommended to pick a high power of 2
+   * @throws IllegalArgumentException, If a paramter is not what is expected
+   * @return void
+   */
+	public static void collatzConjectureEfficent (long start, long end, int iterationToPrint) {
+		// Checking if start is valid
+		if (start < 1) throw new IllegalArgumentException("Start parameter must be greater than 0");
+
+		// Checking if end in valid
+		if (end < 0) throw new IllegalArgumentException("End parameter must be positive");
+
+		// If end is 0, set end to its maximum value
+		if (end == 0) end = Long.MAX_VALUE;
+
+		// Checking if interationToPrint is vaild
+		if (iterationToPrint < 1) throw new IllegalArgumentException("interationToPrint parameter must be greater than 0");
+
+		// If start is even, make it odd by subtracting by 1 for the for-loop later
+		if (start % 2 == 0) start -= 1;
+
+		// This for-loop efficantly goes through every Collatz number to check if it goes to 1.
+		// Skips even numbers because even numbers will be divided by 2, making the number one that has already been checked.
+		for (long collatzNum = start; collatzNum <= end; collatzNum += 2) {
+
+			// This for-loop efficantly iterates through the starting number until the number is garrenteed to go to 1
+			// If during the iterations, the number goes below the starting number it breaks the loop since that number has already been checked
+			for (long i = collatzNum; i != 1;) {
+
+				// If even, divide by 2, else (odd), times by 3 and add one.
+				if (i % 2 == 0) {
+					i = i / 2;
+				} else {
+					i = (3 * i) + 1;
+				}
+
+				// If i is ever less than the collatzNum, i is garenteed to go to 1 since that number has been checked
+				if (i < collatzNum) break;
+			}
+
+			// If the collatzNum is divisable by iterationToPrint, print the collatzNum
+			if (collaztNum % iterationToPrint == 0) System.out.println("Number Tested: " + collatzNum);
+	
+		}
+
+		// Simple print statement to indicate method is done
+		System.out.println("Method finished running.");
+
+	}
 }
