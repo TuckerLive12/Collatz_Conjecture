6272eafafab5f89ccff7934f970282ee6a9a8bc6
TuckerLive12 <Jack.kucera@me.com>
2025-05-19 14:11:23 -0500

    small tweaks

--- CollatzConjecture.java
+++ CollatzConjecture.java
@@ -18,39 +18,39 @@ public class CollatzConjecture {
    * @param data, an integer to put into the new node
    * @return void
    */
-    public static void collatzConjecture(long start, long end, boolean write, boolean showeachoperation) throws IOException {
-			File file = new File("Collatz_Conjecture/CollatzConjecture.txt");
-			FileWriter fwriter = new FileWriter(file, true);
-			if (!write) fwriter.close();
-			if (write) {
-				FileWriter clear = new FileWriter(file, false);
-				clear.write("");
-				clear.close();
-			}
+    public static void collatzConjecture(long start, long end, boolean write, boolean showEachOperation) throws IOException {
+		File file = new File("Collatz_Conjecture/CollatzConjecture.txt");
+		FileWriter fwriter = new FileWriter(file, true);
+		if (!write) fwriter.close();
+		if (write) {
+			FileWriter clear = new FileWriter(file, false);
+			clear.write("");
+			clear.close();
+		}
 		
 
         long numOp;
-			for (long collatzNum = start; collatzNum <= end; collatzNum++) {
-				numOp = 0;
-				if (showeachoperation) {
-					System.out.println("Starting number: " + collatzNum);
+		for (long collatzNum = start; collatzNum <= end; collatzNum++) {
+			numOp = 0;
+			if (showEachOperation) {
+				System.out.println("Starting number: " + collatzNum);
+			}
+			for (long i = collatzNum; i != 1;) {
+				if (i % 2 == 0) {
+					i = i / 2;
+				} else {
+					i = (3 * i) + 1;
 				}
-				for (long i = collatzNum; i != 1;) {
-					if (i % 2 == 0) {
-						i = i / 2;
-					} else {
-						i = (3 * i) + 1;
-					}
-					numOp += 1;
-					if (showeachoperation) {
-						System.out.println("Operation: " + numOp + ", Current number: " + i);
-					}
-				}
-				//System.out.println("Starting number: " + collatzNum + "\nNumber of Operations: " + numOp + "\n");
-				if (write) {
-					fwriter.write(collatzNum + ": " + numOp +"\n");
+				numOp += 1;
+				if (showEachOperation) {
+					System.out.println("Operation: " + numOp + ", Current number: " + i);
 				}
 			}
-			fwriter.close();
+			//System.out.println("Starting number: " + collatzNum + "\nNumber of Operations: " + numOp + "\n");
+			if (write) {
+				fwriter.write(collatzNum + ": " + numOp +"\n");
+			}
+		}
+		fwriter.close();
     }
 }
