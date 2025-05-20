78b27ca47a70763c12ae6f3dafbef70bc6e9c136
TuckerLive12 <Jack.kucera@me.com>
2025-05-19 19:39:59 -0500

    tweeked methods in CollatzConjecture, added some time measurment in collatzConjectureEfficentTest. Added a commented out time measurment in Driver.

--- CollatzConjecture.java
+++ CollatzConjecture.java
@@ -1,13 +1,13 @@
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+import java.util.ArrayList;
 
 /**
-* Contains code for a singly Linked List. Contains two inner classes, SingleLinkNode and
-*  Iterator. Implements the methods addNode, removeNode, printList, and size.
+* Contains static methods to test and write for stats the Collatz Conjecture.
 *
-* Last Modified: May 9, 2025
-* @auther Jack Kucera
+* Last Modified: May 19, 2025
+* @auther Jack Kucera, Will Kucera
 **/
 public class CollatzConjecture {
 
@@ -22,6 +22,7 @@ public class CollatzConjecture {
    * @throws IOException, If an IOExcpetion has taken place
    * @return void
    */
+    @SuppressWarnings("ConvertToTryWithResources")
     public static void collatzConjecture(long start, long end, boolean write, boolean showEachOperation) throws IOException {
 		File file = new File("Collatz_Conjecture/CollatzConjecture.txt");
 		FileWriter fwriter = new FileWriter(file, true);
@@ -63,11 +64,11 @@ public class CollatzConjecture {
    *
    * @param start, A long which indicates what number to start testing the Collatz Conjecture
    * @param end, A long which indicates what number to end testing the Collatz Conjecture. If end is 0, never stops.
-   * @param iterationToPrint, An int which indicates after how many iterations to print the numbering being tested. Does this by modulating the number being tested, if 0, print. Recommended to pick a high power of 2, 2^24 is good.
+   * @param iterationToPrint, An int which indicates after how many iterations to print the numbering being tested. If 0, never print. Recommended to pick a number greater than 100,000,000.
    * @throws IllegalArgumentException, If a paramter is not what is expected
    * @return void
    */
-	public static void collatzConjectureEfficent (long start, long end, int iterationToPrint) {
+	public static void collatzConjectureEfficent (long start, long end, long iterationToPrint) {
 		// Checking if start is valid
 		if (start < 1) throw new IllegalArgumentException("Start parameter must be greater than 0");
 
@@ -78,18 +79,35 @@ public class CollatzConjecture {
 		if (end == 0) end = Long.MAX_VALUE;
 
 		// Checking if interationToPrint is vaild
-		if (iterationToPrint < 1) throw new IllegalArgumentException("interationToPrint parameter must be greater than 0");
+		if (iterationToPrint < 0) throw new IllegalArgumentException("interationToPrint parameter must be positive");
 
 		// If start is even, make it odd by subtracting by 1 for the for-loop later
 		if (start % 2 == 0) start -= 1;
+
+		// If iterationToPrint equals 0, make it never print
+		if (iterationToPrint == 0) {
+			iterationToPrint = iterationToPrint-1; // This will make whenPrint == iterationToPrint never possible
+		
+		// Else if iterationToPrint equals 1, set iterationToPrint to 1
+		} else if (iterationToPrint == 1) {
+			iterationToPrint = 1;
+
+		// Else, divide iterationToPrint by 2 for collatz number being tested and iterations to match
+		} else {
+			iterationToPrint /= 2;
+		}
+
+		// For printing numbers in for-loop
+		long whenPrint = 0;
 
 		// This for-loop efficantly goes through every Collatz number to check if it goes to 1.
 		// Skips even numbers because even numbers will be divided by 2, making the number one that has already been checked.
 		for (long collatzNum = start; collatzNum <= end; collatzNum += 2) {
 
-			// This for-loop efficantly iterates through the starting number until the number is garrenteed to go to 1
-			// If during the iterations, the number goes below the starting number it breaks the loop since that number has already been checked
-			for (long i = collatzNum; i != 1;) {
+			// This while-loop efficantly iterates through the starting number until the number is garrenteed to go to 1
+			// If during the iterations i goes below collatzNum, it ends the loop since that number has already been checked
+			long i = collatzNum; 
+			while (i > collatzNum) { // Equivalent to if (i < collatzNum) break;
 
 				// If even, divide by 2, else (odd), times by 3 and add one.
 				if (i % 2 == 0) {
@@ -97,18 +115,124 @@ public class CollatzConjecture {
 				} else {
 					i = (3 * i) + 1;
 				}
-
-				// If i is ever less than the collatzNum, i is garenteed to go to 1 since that number has been checked
-				if (i < collatzNum) break;
 			}
 
-			// If the collatzNum is divisable by iterationToPrint, print the collatzNum
-			if ((collatzNum + 1) % iterationToPrint == 0) System.out.println("Number Tested: " + String.format("%,d", collatzNum));
+			
+
+			// If whenPrint equals iterationToPrint, print collatzNum tested and set whenPrint to 0.
+			if (whenPrint == iterationToPrint) {
+				System.out.println("Number Tested: " + String.format("%,d", collatzNum));
+				whenPrint = 0;
+			} 
+
+			// whenPrint goes up by 1 every loop
+			whenPrint++;	
 	
 		}
 
 		// Simple print statement to indicate method is done
-		System.out.println("Method finished running.");
+		System.out.println("Method finished running. Final Number Tested: " + String.format("%,d", end));
+
+	}
+
+	/**
+   * This method is for testing different code for collatzConjectureEfficent()
+   *
+   * @param start, A long which indicates what number to start testing the Collatz Conjecture
+   * @param end, A long which indicates what number to end testing the Collatz Conjecture. If end is 0, never stops.
+   * @param iterationToPrint, An int which indicates after how many iterations to print the numbering being tested. If 0, never print. Recommended to pick a number greater than 100,000,000.
+   * @throws IllegalArgumentException, If a paramter is not what is expected
+   * @return void
+   */
+	public static void collatzConjectureEfficentTest (long start, long end, long iterationToPrint) {
+		// Checking if start is valid
+		if (start < 1) throw new IllegalArgumentException("Start parameter must be greater than 0");
+
+		// Checking if end in valid
+		if (end < 0) throw new IllegalArgumentException("End parameter must be positive");
+
+		// If end is 0, set end to its maximum value
+		if (end == 0) end = Long.MAX_VALUE;
+
+		// Checking if interationToPrint is vaild
+		if (iterationToPrint < 0) throw new IllegalArgumentException("interationToPrint parameter must be positive");
+
+		// If start is even, make it odd by subtracting by 1 for the for-loop later
+		if (start % 2 == 0) start -= 1;
+
+		// If iterationToPrint equals 0, make it never print
+		if (iterationToPrint == 0) {
+			iterationToPrint = iterationToPrint-1; // This will make whenPrint == iterationToPrint never possible
+		
+		// Else if iterationToPrint equals 1, set iterationToPrint to 1
+		} else if (iterationToPrint == 1) {
+			iterationToPrint = 1;
+
+		// Else, divide iterationToPrint by 2 for collatz number being tested and iterations to match
+		} else {
+			iterationToPrint /= 2;
+		}
+
+		// For printing numbers in for-loop
+		long whenPrint = 0;
+
+		// For testing runtime
+		ArrayList<Long> milliseconds = new ArrayList<Long>(1000);
+		long endTime;
+		long startTime = System.nanoTime();
+		
+		// This for-loop efficantly goes through every Collatz number to check if it goes to 1.
+		// Skips even numbers because even numbers will be divided by 2, making the number one that has already been checked.
+		for (long collatzNum = start; collatzNum <= end; collatzNum += 2) {
+	
+
+			// This while-loop efficantly iterates through the starting number until the number is garrenteed to go to 1
+			// If during the iterations i goes below collatzNum, it ends the loop since that number has already been checked
+			long i = collatzNum; 
+			while (i > collatzNum) { // Equivalent to if (i < collatzNum) break;
+
+				// If even, divide by 2, else (odd), times by 3 and add one.
+				if (i % 2 == 0) {
+					i = i / 2;
+				} else {
+					i = (3 * i) + 1;
+				}
+			}
+
+			
+
+			// If whenPrint equals iterationToPrint, print collatzNum tested and set whenPrint to 0.
+			if (whenPrint == iterationToPrint) {
+				// For testing runtime
+				endTime = System.nanoTime();
+				long duration = (endTime - startTime) / 1_000_000;
+				System.out.println("Execution time: " + duration + " ms");
+				milliseconds.add(duration);
+
+				System.out.println("Number Tested: " + String.format("%,d", collatzNum));
+				whenPrint = 0;
+
+				// For testing runtime
+				startTime = System.nanoTime();
+			} 
+
+			// whenPrint goes up by 1 every loop
+			whenPrint += 1;	
+	
+		}
+
+		// For testing runtime
+		long average = 0;
+		
+		for (long num : milliseconds) {
+			average += num;
+		}
+
+		System.out.println("Total Execution time: " + average + " ms");
+		System.out.println("Average Execution time: " + average/milliseconds.size() + " ms");
+
+		// Simple print statement to indicate method is done
+		System.out.println("Method finished running. Final Number Tested: " + String.format("%,d", end));
 
 	}
 }
--- Driver.java
+++ Driver.java
@@ -4,7 +4,16 @@ import java.util.Scanner;
 //went to 10 billion
 public class Driver {
 
+    @SuppressWarnings("ConvertToTryWithResources")
 	public static void main(String[] args) throws IOException {
+
+		// 1-1,000,000,000,000 :
+		// Total: 2971678 ms, Average 2974 ms
+		//
+		// 1,000,000,000,000-2,000,000,000,000:
+		// Total: 2851900 ms, Average 2854 ms
+		CollatzConjecture.collatzConjectureEfficent(1,0, 10_000_000_000L);
+		System.exit(0);
 
 		Scanner input = new Scanner(System.in);
 
@@ -24,5 +33,46 @@ public class Driver {
 		CollatzConjecture.collatzConjecture(startNum,desiredNum,write,showOp);
 
 		System.out.println("Program Done!");
+
+
+		// This commented code chunk is for testing the runtime of different methods
+		// NO NOT DELETE
+		/** 
+		CollatzConjecture.collatzConjectureEfficent(1,1_000_000_000, 100_000_000);
+		CollatzConjecture.collatzConjectureEfficent(1,1_000_000_000, 100_000_000);
+
+		long[] milliseconds = new long[100];
+		long average1 = 0;
+		for (int i = 0; i < milliseconds.length; i++) {
+			long startTime = System.nanoTime();
+				CollatzConjecture.collatzConjectureEfficent(1,1_000_000_000, 1_000_000_000);
+			long endTime = System.nanoTime();
+			long duration = (endTime - startTime) / 1_000_000; // Convert to milliseconds
+			System.out.println("Execution time: " + duration + " ms");
+			milliseconds[i] = duration;	
+		}
+		for (long num : milliseconds) {
+			average1 += num;
+		}
+		
+
+		long average2 = 0;
+		for (int i = 0; i < milliseconds.length; i++) {
+			long startTime = System.nanoTime();
+				CollatzConjecture.collatzConjectureEfficentTest(1,1_000_000_000, 1_000_000_000);
+			long endTime = System.nanoTime();
+			long duration = (endTime - startTime) / 1_000_000; // Convert to milliseconds
+			System.out.println("Execution time: " + duration + " ms");
+			milliseconds[i] = duration;	
+		}
+		for (long num : milliseconds) {
+			average2 += num;
+		}
+		System.out.println("Average Execution time: " + average1/milliseconds.length + " ms");
+		System.out.println("Average Execution time: " + average2/milliseconds.length + " ms");
+
+		// Stops running code
+		System.exit(0);
+		*/
 	}
 }   
